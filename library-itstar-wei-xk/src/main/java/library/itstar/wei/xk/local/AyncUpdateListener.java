b8d4a657a63eccefe9aa44502d9bae57991f3c26
itstart-jason <jason.yang.itstar@gmail.com>
2018-05-08 17:52:27 +0800

    library xwalkview

--- library-itstar-wei-xk/src/main/java/library/itstar/wei/xk/local/AyncUpdateListener.java
+++
@@ -1,6 +0,0 @@
-package library.itstar.wei.xk.local;
-
-public interface AyncUpdateListener
-{
-    public void onProgressUpdate ( String response );
-}
--- library-itstar-wei-xk/src/main/java/library/itstar/wei/xk/local/CallBackListener.java
+++
@@ -1,6 +0,0 @@
-package library.itstar.wei.xk.local;
-
-public interface CallBackListener
-{
-    public void onTaskCompleted ( String response );
-}
